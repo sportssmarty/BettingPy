8687e7a3323f1b6c756e804248c73fe0187bce0b
PJ Fitzpatrick <fitzpatrick.pj@googlemail.com>
2013-10-12 19:37:21 +0100

    Update feedutil.py

--- feedutil.py
+++ feedutil.py
@@ -1,4 +1,4 @@
-/* ==========================================================
+"""
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -10,7 +10,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ========================================================== */
+"""
 import os
 from soccerstatic import completetourmap,completeteammap
 from matchidutil import getdiv,get_match_teams
